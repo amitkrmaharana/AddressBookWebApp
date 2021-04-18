8476a1f40f8bb096c48558084427d8e458f3e6f1
amitkrmaharana <ak657harley@gmail.com>
2021-04-19 03:37:42 +0530

    Refactored: Viewed the Local storage in a tabular format

--- js/homePage.js
+++ js/homePage.js
@@ -8,9 +8,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const createInnerHtml = () => {
     if (addressBookList.length == 0) return;
-    const headerHtml = "<th>Fullname</th><th>Address</th><th>City</th><th>State</th>" +
-        "<th>Zip Code</th><th>Phone Number</th><th></th>";
-    let innerHtml = `$(headerHtml)`;
+    const headerHtml = "<th>Fullname</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th></th>";
+    let innerHtml = `${headerHtml}`;
     for (const addressBookData of addressBookList) {
         innerHtml = `${innerHtml}
             <tr>
@@ -19,7 +18,7 @@ const createInnerHtml = () => {
                 <td>${addressBookData._city}</td>
                 <td>${addressBookData._state}</td>
                 <td>${addressBookData._zip}</td>
-                <td>${addressBookData.phoneNumber}</td>
+                <td>${addressBookData._phoneNumber}</td>
                 <td>
                     <img id="${addressBookData._id}" alt="delete" onclick="remove(this)" src="../assets/icon/delete-black-18dp.svg">
                     <img id="${addressBookData._id}" alt="edit" onclick="update(this)" src="../assets/icon/create-black-18dp.svg">                    
