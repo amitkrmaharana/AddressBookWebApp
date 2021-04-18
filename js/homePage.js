ff312d790609addd7df000dcff98f3e303f1cc18
amitkrmaharana <ak657harley@gmail.com>
2021-04-19 04:40:12 +0530

    Refactored: refactored to add remove functionality

--- js/homePage.js
+++ js/homePage.js
@@ -10,7 +10,7 @@ const createInnerHtml = () => {
     if (addressBookList.length == 0) return;
     const headerHtml = "<th>Fullname</th><th>Address</th><th>City</th><th>State</th>" +
         "<th>Zip Code</th><th>Phone Number</th><th></th>";
-    let innerHtml = `$(headerHtml)`;
+    let innerHtml = `${headerHtml}`;
     for (const addressBookData of addressBookList) {
         innerHtml = `${innerHtml}
             <tr>
@@ -19,13 +19,24 @@ const createInnerHtml = () => {
                 <td>${addressBookData._city}</td>
                 <td>${addressBookData._state}</td>
                 <td>${addressBookData._zip}</td>
-                <td>${addressBookData.phoneNumber}</td>
+                <td>${addressBookData._phoneNumber}</td>
                 <td>
-                    <img id="${addressBookData._id}" alt="delete" onclick="remove(this)" src="../assets/icon/delete-black-18dp.svg">
+                    <img id="${addressBookData._id}" alt="delete"  src="../assets/icon/delete-black-18dp.svg" onclick="remove(this)">
                     <img id="${addressBookData._id}" alt="edit" onclick="update(this)" src="../assets/icon/create-black-18dp.svg">                    
                 </td>
             </tr>
         `;
     }
     document.querySelector('#table-display').innerHTML = innerHtml;
-}
+};
+
+const remove = (node) => {
+    let addressBookData = addressBookList.find((contact) => contact._id == node.id);
+    if (!addressBookData) return;
+    const index = addressBookList.map((contact) => contact._id)
+        .indexOf(addressBookData._id);
+    addressBookList.splice(index, 1);
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+    document.querySelector(".contact-count").textContent = addressBookList.length;
+    createInnerHtml();
+};
